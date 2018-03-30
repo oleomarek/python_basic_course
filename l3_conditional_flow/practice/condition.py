47d5ced1baee74891bf1d6f6d8f1267b8f626723
oleomarek <37668665+oleomarek@users.noreply.github.com>
2018-03-30 08:39:07 +0300

    Hw3 (#2)
    hw3

--- l3_conditional_flow/practice/condition.py
+++ l3_conditional_flow/practice/condition.py
@@ -3,7 +3,7 @@ age = input("\n" "Enter your age:\n")
 email = print("\n"f'Your email {name + age}@itea.ua' '\n')
 try:
     name/input("Enter something value:\n")
-    surname = print("\n""Noob")
+    surname = print("\nNoob")
 except TypeError:
     surname = print("\n""Marlin")
 print("\nDeveloping with Python...\n")
@@ -12,3 +12,16 @@ if surname=="Noob":
     print("Ti dopustil oshibku!")
 else:
     print("Molodec")
+
+### К домашнему заданию:
+
+# number=input("Enter Credit card number!\n(Attention: 16 digits.):\n\n")
+# long=len(number)
+# try:
+#     number=int(number)
+#     if long!=16 or number<0:
+#         print("!!!")
+#         exit()                              #можно воспользоваться таким вариантом:   raise ValueError("Long format")
+# except ValueError:                          #можно воспользоваться таким вариантом:   except ValueError as error:
+#     print("Not the correct format!")        #можно воспользоваться таким вариантом:   print("Not the correct format!" + repr(error))
+#     exit()
