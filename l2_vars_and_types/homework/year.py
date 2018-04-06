3db30c2785aa0ccf6de3767b143486c8564a48ca
oleomarek <37668665+oleomarek@users.noreply.github.com>
2018-04-06 09:22:13 +0300

    Delete year.py

--- l2_vars_and_types/homework/year.py
+++
@@ -1,7 +0,0 @@
-age = int(input('Enter your age:\n\n'))
-print(f'\nYou were born in {2018 - age} year!')
-
-#date = map(int(input("Введи месяц: "))/split())
-# mm, yy = input("Experation date your card: mm/yy\n").split("/")
-#print(mm, yy)
-
