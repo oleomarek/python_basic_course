0f9f373c8d4ded881990ea087ea9d48b3ba919a0
oleomarek <37668665+oleomarek@users.noreply.github.com>
2018-05-08 11:57:17 +0300

    Done final project! (#10)
    * hw2
    
    * hw3
    
    * hw4
    
    * hw5
    
    * hw8-final project
    
    * changed with recommendations

---
+++ final_project/app.py
@@ -0,0 +1,25 @@
+import csv
+
+from python_basic_course.final_project.utils import *
+
+    #generetad list of 100 dictionaries
+ls = []
+for i in range(100):
+    ls.append(generated_input())
+
+    #create files .csv across call function 'check_credents':
+with open ("cardholders.csv", "w", newline= "") as cardholders_csv:
+    header = ["Name", "Phone", "Exp_day", "Card"]
+    writer = csv.DictWriter(cardholders_csv, fieldnames= header)
+    writer.writeheader()
+    for i in range(len(ls)):
+        if check_credents(ls[i]):
+            writer.writerow({"Name": ls[i].get("Name"), "Phone": ls[i].get("Phone"),"Exp_day": ls[i].get("Exp_day"), "Card": ls[i].get("Card")})
+
+with open("cardholders_errors.csv", "w", newline="") as cardholders_error_csv:
+    header = ["Name", "Phone", "Exp_day", "Card"]
+    writer = csv.DictWriter(cardholders_error_csv, fieldnames= header)
+    writer.writeheader()
+    for i in range(len(ls)):
+        if not check_credents(ls[i]):
+            writer.writerow({"Name": ls[i].get("Name"), "Phone": ls[i].get("Phone"), "Exp_day": ls[i].get("Exp_day"), "Card": ls[i].get("Card")})
